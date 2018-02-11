4f42102e3f8f2975ca2c326825d69daf593ec326
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2018-02-12 00:51:52 +0200

    valid palindrome

---
+++ leetcode/two pointers/meeting array/valid_palindrome.py
@@ -0,0 +1,23 @@
+# description : https://leetcode.com/problems/valid-palindrome/description/ # NOQA
+# author : Hussam
+# solution : two pointers
+# o(n) time, o(1) space
+
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
